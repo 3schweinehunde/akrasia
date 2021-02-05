c09549bb250961d53509f1eb0980a0838f7158f9
stefan Haslinger <mail@stefan-haslinger.at>
2021-02-05 15:07:59 +0100

    updated dependencies

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,13 +20,13 @@ import 'alpinejs'
 import ApexCharts from 'apexcharts'
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, { 
-  params: {_csrf_token: csrfToken }, 
+let liveSocket = new LiveSocket("/live", Socket, {
   dom: {
     onBeforeElUpdated(from, to){
       if(from.__x){ window.Alpine.clone(from.__x, to) }
     }
   },
+  params: {_csrf_token: csrfToken}
 })
 
 // Show progress bar on live navigation and form submits
