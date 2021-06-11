d1d409e78599534690271c2c65f095025c690ade
Stefan Haslinger <stefan.haslinger@informatom.com>
2021-06-11 10:36:54 +0200

    missing ,

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :akrasia, AkrasiaWeb.Endpoint,
   http: [port: 9999, compress: false],
   url: [scheme: "https", host: "akrasia.informatom.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   root: ".",
   check_origin: ["https://akrasia.informatom.com"],
   version: Mix.Project.config()[:version]
