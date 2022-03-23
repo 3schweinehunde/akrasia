aeb1c41972e3707bf65e39e0e499373c6f659b0e
Stefan Haslinger <mail@stefan-haslinger.at>
2022-03-23 15:57:25 +0100

    esbuild config: version, paths, especially: fonts

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,8 @@ config :akrasia, Akrasia.Mailer,
 config :esbuild,
   version: "0.14.27",
   default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
